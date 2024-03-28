f8fc70125550ff81c65e898d2c5ed005884a8ece
Avishek Sen <x0rzavi@gmail.com>
2024-03-28 10:53:06 +0530

    update logic

--- main.js
+++ main.js
@@ -1,48 +1,39 @@
-require('dotenv').config();
+// require('dotenv').config();
 const qrcode = require('qrcode-terminal');
 
 const { Client, LocalAuth } = require('whatsapp-web.js');
 
 const client = new Client({
-        authStrategy: new LocalAuth()
+  authStrategy: new LocalAuth()
 });
 
 client.on('loading_screen', (percent, message) => {
-        console.log('Loading:', percent, message);
+  console.log('Loading:', percent, message);
 });
 
 client.on('qr', qr => {
-        qrcode.generate(qr, {small: true});
+  qrcode.generate(qr, {small: true});
 });
 
 client.on('ready', () => {
-        console.log('Client is ready!\n');
+  console.log('Client is ready!\n');
 });
 
 client.on('message_revoke_everyone', async (after, before) => {
-        if (before.fromMe == true) {
-                const chatName = process.env.CHAT_NAME;
-                const chats = await client.getChats();
-
-                if (process.env.CHAT_ID) {
-                        chatId = process.env.CHAT_ID
-                } else {
-                        for (const chat of chats) {
-                                if (chat.name === chatName) {
-                                        chatId = chat.id._serialized
-                                        console.log(`Save to .env file`)
-                                        console.log(`CHAT_ID='${chat.id._serialized}'`);
-                                        break;
-                                }
-                        }
-                }
-
-                const sender = await before.getContact();
-                const msg = sender.name + " deleted: " + before.body
-                client.sendMessage(chatId, msg);
-                // console.log(before);
-        }
+  if (before) {
+    if (before.fromMe == false) {
+      const sender = await before.getContact();
+      const sender_name = sender.name;
+      const sender_number = sender.number;
+      const sender_id = sender.pushname;
+      msg = "Name: " + sender_name + "\n";
+      msg += "Number: +" + sender_number + "\n";
+      msg += "UserName: " + sender_id + "\n";
+      msg += "Deleted: \n" + before.body;
+      console.log(msg, "\n");
+      client.sendMessage(client.info.me._serialized, msg);
+    }
+  }
 });
-
 
 client.initialize();
